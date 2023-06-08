13b220c3be765ecda6700d04969f0c2910b554fa
Michael K. Ocansey <kabutey@gmail.com>
2023-06-08 15:52:04 +0000

    Update ads.blade.php

--- resources/views/docs/ads.blade.php
+++ resources/views/docs/ads.blade.php
@@ -1,4 +1,4 @@
 <div class="pt-16 pb-4 text-xs opacity-70">Supported by</div>
 <a href="https://jb.gg/OpenSourceSupport" target="_blank">
-    <img src="{{ asset('assets/images/phpstorm.svg')  }}" alt="PhpStorm Logo" class="w-36 dark:bg-white/20 dark:p-2" />
+    <img src="{{ asset('assets/images/PhpStorm.svg')  }}" alt="PhpStorm Logo" class="w-36 dark:bg-white/20 dark:p-2" />
 </a>
