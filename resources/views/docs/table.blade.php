10939276955ffbd7a56e260f46fe4388b4354cd9
Michael K. Ocansey <kabutey@gmail.com>
2023-09-02 22:00:14 +0000

    WIP: documentation for building table from dynamic data

--- resources/views/docs/table.blade.php
+++ resources/views/docs/table.blade.php
@@ -316,6 +316,128 @@
         </code>
     </pre>
 
+    <h2 id="dynamic">Display a Table From Dynamic Data</h2>
+    <p>
+        There is no point manually building a table tediously when you have an array that contains everything you want to display as a table.
+        The table component has a <code class="inline text-red-500">data</code> attribute that accepts a json encoded array. The component builds its
+        table headings using the array keys.
+    </p>
+    <p>
+        Let us consider the array below and its resultant table.
+    </p>
+    <pre class="language-js line-numbers">
+        <code>
+            $staff = [
+                [   'id' => 1,
+                    'first_name' => 'Michael',
+                    'last_name' => 'Ocansey',
+                    'department' => 'Engineering',
+                    'marital_status' => 1
+                ],
+                [
+                    'id' => 2,
+                    'first_name' => 'Alfred',
+                    'last_name' => 'Rowe',
+                    'department' => 'Engineering',
+                    'marital_status' => 1
+                ],
+                ;[
+                    'id' => 3,
+                    'first_name' => 'Abigail',
+                    'last_name' => 'Edwin',
+                    'department' => 'Engineering',
+                    'marital_status' => 0
+                ],
+            ];
+        </code>
+    </pre>
+    <pre class="language-markup line-numbers">
+        <code>
+            &lt;x-bladewind::table data="&#123;&#123; json_encode($staff) }}" /&gt;
+        </code>
+    </pre>
+    @php
+        $staff = [
+            [ 'id' => 1, 'first_name' => 'Michael', 'last_name' => 'Ocansey',   'department' => 'Engineering', 'marital_status' => 1 ],
+            [ 'id' => 2, 'first_name' => 'Alfred',  'last_name' => 'Rowe',     'department' => 'Engineering', 'marital_status' => 1 ],
+            [ 'id' => 3, 'first_name' => 'Abigail',   'last_name' => 'Edwin',    'department' => 'Engineering', 'marital_status' => 0 ],
+        ];
+        $action_icons = [
+            "icon:chat-bubble-bottom-center-text | tip:send user a message | click:sendMessage({first_name}, {last_name})",
+            "icon:pencil | click:redirect(/user/{id})",
+            "icon:trash | color:red| click:deleteUser({id}, {first_name})",
+        ];
+    @endphp
+    <p>
+        <x-bladewind::table data="{{ json_encode($staff) }}" />
+    </p>
+    <p>
+        As you can tell from the above example, the component simply picks the array passed to it and generates a table.
+        You can apply all the <a href="#attributes">table attributes</a> to remove the hover effect, remove the gaps or even make the table striped.
+    </p>
+    <h3>Excluding and Including Columns</h3>
+    <p>
+        By default, the table picks the keys of the array and generates its table headings.
+        Any key with underscores will be replaced with spaces. <b>first_name</b> becomes <b>first name</b>.
+        An API will return staff data with the ID of each user, but it is rare to see the IDs displayed in  a table.
+        The table component allows you to exclude some columns using the <code class="text-red-500 inline">exclude_columns</code> attribute.
+        There is also the <code class="text-red-500 inline">include_columns</code> attribute that lets you specify the only columns to be displayed.
+    </p>
+    <p>
+        If you have an array with 20 fields and you want to display 5 out of the 20 fields, it will be easier to specify the 5 columns in the <code class="inline text-red-500">include_columns</code>
+        attribute rather than specifying 15 columns in the <code class="inline text-red-500">exclude_columns</code> attribute.
+        Let us exclude <code class="inline">id</code> and <code class="inline">marital_status</code> from our table above.
+    </p>
+    <p>
+        <x-bladewind::alert show_close_icon="false" show_icon="false">
+            <code class="text-red-500 inline">include_columns</code> takes precedence over <code class="text-red-500 inline">exclude_columns</code>. If you specify both attributes, <code class="text-red-500 inline">exclude_columns</code> will be ignored.
+        </x-bladewind::alert>
+    </p>
+    <x-bladewind::table data="{{ json_encode($staff) }}" exclude_columns="id, marital_status" />
+    <pre class="language-markup line-numbers" data-line="2">
+        <code>
+            &lt;x-bladewind::table
+                exclude_columns="id, marital_status"
+                data="&#123;&#123;json_encode($staff) }}" /&gt;
+        </code>
+    </pre>
+    <h3>Displaying Action Icons</h3>
+    <p>
+        Using the table component with dynamic data allows you to specify some extra cool attributes.
+        You can show action icons by passing a json encoded array in the <code class="text-red-500 inline">action_icons</code> attribute.
+        Each action icon can have the name of the icon, the icon colour (default is the <a href="/component/button#secondary">secondary button</a> colour), a tooltip and the click action of the icon.
+        The icons will be displayed in the order they are entered into the array.
+    </p>
+    <pre class="language-js line-numbers">
+        <code>
+            $action_icons = [
+                'icon:chat-bubble-bottom-center-text | tip:send user a message | click:sendMessage(first_name, last_name)',
+                'icon:pencil | click:/user/{id}',
+                'icon:trash | color:red | click:deleteUser(id, first_name, last_name)',
+            ];
+        </code>
+    </pre>
+    <script>
+        sendMessage = () => {
+            alert('first_name');
+        }
+        deleteUser = () => {
+            alert('are you deleting the user');
+        }
+        redirect = () => {
+            alert('send away problems');
+        }
+    </script>
+    <pre class="language-markup line-numbers" data-line="2,4">
+        <code>
+            &lt;x-bladewind::table
+                exclude_columns="id, marital_status"
+                data="&#123;&#123; json_encode($staff) }}"
+                action_icons="&#123;&#123; json_encode($action_icons) }}" /&gt;
+        </code>
+    </pre>
+    <x-bladewind::table data="{{ json_encode($staff) }}" action_icons="{{ json_encode($action_icons) }}"  exclude_columns="id, marital_status" />
+
     <h2 id="attributes">Full List Of Attributes</h2>
     <p>The table below shows a comprehensive list of all the attributes available for the Table component.</p>
     @include('docs/announcement')
@@ -398,6 +520,7 @@
         <div class="flex items-center"><div class="dot"></div><a href="#striped">Striped table</a></div>
         <div class="flex items-center"><div class="dot"></div><a href="#totals">Cells for totals</a></div>
         <div class="flex items-center"><div class="dot"></div><a href="#shadow">With drop shadow</a></div>
+        <div class="flex items-center"><div class="dot"></div><a href="#dynamic">Dynamic data</a></div>
         <div class="flex items-center"><div class="dot"></div><a href="#attributes">Full list of attributes</a></div>
     </x-slot:side_nav>
     <x-slot name="scripts">
