17d93e59ac249ba561542406cbb33e620d5676d5
Michael K. Ocansey <kabutey@gmail.com>
2024-03-10 20:31:14 +0000

    Updated contribution guide

--- resources/views/docs/contribute.blade.php
+++ resources/views/docs/contribute.blade.php
@@ -10,7 +10,7 @@
         To help us keep all bug reports and questions consolidated, and to also serve as a reference for others who might have the same questions or reports, we encourage you to post all questions and bug reports on our <a href="https://github.com/mkocansey/bladewind/issues" target="_blank">GitHub issues page</a>.
         Bug reports should be descriptive enough to be reproducible. It is good practice to include the following in your bug report.
     </p>
-    <ul class="list-disc list-inside leading-9 mb-6">
+    <ul class="list-disc list-outside  ml-3.5 !text-slate-600 leading-9 mb-6">
         <li>BladewindUI version number</li>
         <li>PHP version</li>
         <li>OS</li>
@@ -22,13 +22,14 @@
     <p>For changes that address core functionality or would require breaking changes (e.g. a major release), it is best to <a href="https://github.com/mkocansey/bladewind/issues">open an Issue</a> to discuss your proposal first. This is not a must but can save time creating and reviewing changes down the line.</p>
     <p>In general, BladewindUI follows the &quot;<a href="https://github.com/susam/gitpr">fork-and-pull</a>&quot; Git workflow</p>
 
-    <ul class="list-disc list-inside leading-9 mb-6">
+    <ul class="list-disc list-outside leading-9 mb-6 ml-3.5 !text-slate-600">
         <li>Fork the repository to your own Github account.</li>
         <li>Clone the project to your machine.</li>
-        <li>Create a branch locally with an <code class="inline">issue</code> prefix followed by issue number.</li>
+        <li>Create a local branch (from the development branch) with an <code class="inline">issue</code> prefix followed by issue number. If what you are working on does not exist on our issues page, please create the issue. If what intend to work on is a feature from our <a href="/roadmap">roadmap</a>, still create the issue and assign the <code class="inline">enhancement</code> label to it. </li>
         <li>Commit changes to the branch following the formatting guidelines specific to this repo (yet to implement <a href="https://styleci.io/" target="_blank">StyleCI</a>).</li>
         <li>Push changes to your fork.</li>
-        <li>Open a PR in <code class="inline">mkocansey/bladewind</code> repository, <code class="inline">main</code> branch.</li>
+        <li>Open a PR against <code class="inline">mkocansey/bladewind</code> repository, <code class="inline">development</code> branch.</li>
+        <li>Once the PR is reviewed and accepted, it will be merged. The code will then make its way into the <code class="inline">main</code> branch and then a release.</li>
     </ul>
 
     <x-bladewind::alert>Every issue or feature to be worked on must be in its own branch with the branch name being the issue number prefixed with 'issue'. For example: if you decide to work on issue #13, your branch name should be <code class="inline">issue-13</code>.</x-bladewind::alert>
@@ -134,7 +135,7 @@ npx mix watch</code></pre>
 
 <h2 id="conduct">Code of Conduct</h2>
 <p>BladewindUI derives its code of conduct from <a href="https://www.ruby-lang.org/en/conduct/">The Ruby Community Conduct Guideline</a> and we advise contributors to be respectful of these.</p>
-<ul class="list-disc list-inside leading-9 mb-6">
+<ul class="list-disc list-outside  ml-3.5 !text-slate-600 leading-9 mb-6">
 <li>Participants will be tolerant of opposing views.</li>
 <li>Participants must ensure that their language and actions are free of personal attacks and disparaging personal remarks.</li>
 <li>When interpreting the words and actions of others, participants should always assume good intentions.</li>
