6263f24699fd91fd1725090afa226442bff4e949
Michael K. Ocansey <kabutey@gmail.com>
2024-11-25 15:08:51 +0000

    update composer and package json files

--- deploy.php
+++ deploy.php
@@ -45,13 +45,28 @@ task('config', [
 
 desc('run composer update');
 task('deploy:run_composer', function(){
+    writeln('RUN COMPOSER ================================================================================');
     cd(get('deploy_path'));
+    run('cp .env current/'); //rm -f composer.lock &&
+    cd(get('deploy_path').'/current');
+    run('touch database/database.sqlite');
+    run('echo "-----------------------------------------------------" && composer update'); //php /usr/bin/
+    run('echo ">>>>>>>" && sudo chgrp -R www-data storage && php artisan key:generate');
+    run('php artisan config:clear && php artisan view:clear');
+    run('php artisan route:clear && php artisan clear-compiled && php artisan optimize');
+    run('php artisan migrate --force');
+    run('sudo chown -R $USER:www-data bootstrap/cache && sudo chown -R $USER:www-data database');
+    run('pwd && npm install');
+    run('npm run build');
+    run('sudo /usr/sbin/service php8.4-fpm reload');
+    /*cd(get('deploy_path'));
+    run('sudo chgrp -R www-data current/database && touch current/database/database.sqlite'); //rm -f composer.lock &&
     run('sudo cp .env current/ && sudo chgrp -R www-data current/storage'); //rm -f composer.lock &&
     cd(get('deploy_path').'/current');
     run('composer update'); //rm -f composer.lock &&  --ignore-platform-reqs --no-scripts // /usr/local/bin/
-    run('php artisan config:clear && php artisan cache:clear && php artisan view:clear && php artisan route:clear && php artisan clear-compiled && php artisan optimize');
-    run('php artisan vendor:publish --provider="Mkocansey\Bladewind\BladewindServiceProvider" --tag=bladewind-components --force && php artisan vendor:publish --provider="Mkocansey\Bladewind\BladewindServiceProvider" --tag=bladewind-public --force');
-    run('sudo /usr/sbin/service php8.1-fpm reload');
+    run('php artisan migrate --force && php artisan config:clear && php artisan cache:clear && php artisan view:clear && php artisan route:clear && php artisan clear-compiled && php artisan optimize');
+    run('php artisan vendor:publish --provider="Mkocansey\Bladewind\BladewindServiceProvider" --tag=bladewind-public --force'); //php artisan vendor:publish --provider="Mkocansey\Bladewind\BladewindServiceProvider" --tag=bladewind-components --force &&
+    run('sudo /usr/sbin/service php8.4-fpm reload');*/
 });
 
 after('deploy:failed', 'deploy:unlock');
