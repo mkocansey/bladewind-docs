85ed57525259128aa176fc7d416639f9709934ac
Michael K. Ocansey <kabutey@gmail.com>
2025-07-16 14:58:59 +0000

    Update bolt icon size in components list
    Changed the bolt icon size from !size-4 to !size-3 for Chart, CheckCard, Filepicker, Table, and Timepicker components. Also added the bolt icon to the Shimmer component for consistency.

--- resources/views/docs/components-list.blade.php
+++ resources/views/docs/components-list.blade.php
@@ -8,14 +8,14 @@
 <div class="{{ $css }} component-button"><div class="dot"></div><a href="/component/button">Button</a></div>
 <div class="{{ $css }} component-card"><div class="dot"></div><a href="/component/card">Card</a></div>
 <div class="{{ $css }} component-centered-content"><div class="dot"></div><a href="/component/centered-content">Centered Content</a></div>
-<div class="{{ $css }} component-chart"><div class="dot"></div><a href="/component/chart">Chart <x-bladewind::icon name="bolt" type="solid" class="text-pink-600 !size-4 ml-1" /></a></div>
+<div class="{{ $css }} component-chart"><div class="dot"></div><a href="/component/chart">Chart <x-bladewind::icon name="bolt" type="solid" class="text-pink-600 !size-3 ml-1" /></a></div>
 <div class="{{ $css }} component-checkbox"><div class="dot"></div><a href="/component/checkbox">Checkbox</a></div>
-<div class="{{ $css }} component-checkcard"><div class="dot"></div><a href="/component/checkcard">CheckCard  <x-bladewind::icon name="bolt" type="solid" class="text-pink-600 !size-4 ml-1" /></a></div>
+<div class="{{ $css }} component-checkcard"><div class="dot"></div><a href="/component/checkcard">CheckCard  <x-bladewind::icon name="bolt" type="solid" class="text-pink-600 !size-3 ml-1" /></a></div>
 <div class="{{ $css }} component-colorpicker"><div class="dot"></div><a href="/component/colorpicker">Colorpicker</a></div>
 <div class="{{ $css }} component-datepicker"><div class="dot"></div><a href="/component/datepicker">Datepicker</a></div>
 <div class="{{ $css }} component-dropmenu"><div class="dot"></div><a href="/component/dropmenu">Dropmenu</a></div>
 <div class="{{ $css }} component-empty-state"><div class="dot"></div><a href="/component/empty-state">Empty State</a></div>
-<div class="{{ $css }} component-filepicker"><div class="dot"></div><a href="/component/filepicker">Filepicker <x-bladewind::icon name="bolt" type="solid" class="text-pink-600 !size-4 ml-1" /></a></div>
+<div class="{{ $css }} component-filepicker"><div class="dot"></div><a href="/component/filepicker">Filepicker <x-bladewind::icon name="bolt" type="solid" class="text-pink-600 !size-3 ml-1" /></a></div>
 <div class="{{ $css }} component-hlg"><div class="dot"></div><a href="/component/horizontal-line-graph">Horizontal Line Graph</a></div>
 <div class="{{ $css }} component-icon"><div class="dot"></div><a href="/component/icon">Icon</a></div>
 <div class="{{ $css }} component-input"><div class="dot"></div><a href="/component/input">Input</a></div>
@@ -29,17 +29,17 @@
 <div class="{{ $css }} component-radio-button"><div class="dot"></div><a href="/component/radio-button">Radio Button</a></div>
 <div class="{{ $css }} component-rating"><div class="dot"></div><a href="/component/rating">Rating</a></div>
 <div class="{{ $css }} component-select"><div class="dot"></div><a href="/component/select">Select</a></div>
-<div class="{{ $css }} component-shimmer"><div class="dot"></div><a href="/component/shimmer">Shimmer</a></div>
+<div class="{{ $css }} component-shimmer"><div class="dot"></div><a href="/component/shimmer">Shimmer <x-bladewind::icon name="bolt" type="solid" class="text-pink-600 !size-3 ml-1" /></a></div>
 <div class="{{ $css }} component-slider"><div class="dot"></div><a href="/component/slider">Slider</a></div>
 <div class="{{ $css }} component-spinner"><div class="dot"></div><a href="/component/spinner">Spinner</a></div>
 <div class="{{ $css }} component-statistic"><div class="dot"></div><a href="/component/statistic">Statistic</a></div>
 <div class="{{ $css }} component-tab"><div class="dot"></div><a href="/component/tab">Tab</a></div>
-<div class="{{ $css }} component-table"><div class="dot"></div><a href="/component/table">Table <x-bladewind::icon name="bolt" type="solid" class="text-pink-600 !size-4" /></a></div>
+<div class="{{ $css }} component-table"><div class="dot"></div><a href="/component/table">Table <x-bladewind::icon name="bolt" type="solid" class="text-pink-600 !size-3" /></a></div>
 <div class="{{ $css }} component-tag"><div class="dot"></div><a href="/component/tag">Tag</a></div>
 <div class="{{ $css }} component-textarea"><div class="dot"></div><a href="/component/textarea">Textarea</a></div>
 <div class="{{ $css }} component-theme-switcher"><div class="dot"></div><a href="/component/theme-switcher">Theme Switcher</a></div>
 <div class="{{ $css }} component-timeline"><div class="dot"></div><a href="/component/timeline">Timeline</a></div>
-<div class="{{ $css }} component-timepicker"><div class="dot"></div><a href="/component/timepicker">Timepicker <x-bladewind::icon name="bolt" type="solid" class="text-pink-600 !size-4 ml-1" /></a></div>
+<div class="{{ $css }} component-timepicker"><div class="dot"></div><a href="/component/timepicker">Timepicker <x-bladewind::icon name="bolt" type="solid" class="text-pink-600 !size-3 ml-1" /></a></div>
 {{--<div class="{{ $css }} component-tooltip hidden"><div class="dot"></div><a href="/component/tooltip">Tooltip</a></div>--}}
 <div class="{{ $css }} component-toggle"><div class="dot"></div><a href="/component/toggle">Toggle</a></div>
 <div class="{{ $css }} component-code"><div class="dot"></div><a href="/component/verification-code">Verification Code</a></div>
